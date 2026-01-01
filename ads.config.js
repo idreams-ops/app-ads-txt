e891fa9a1857f5611ce31dd2923a40fdac61bca6
idreams-ops <idreams.help@gmail.com>
2026-01-01 22:05:25 +0500

    Create ads.config.js

---
+++ ads.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  outputFile: "app-ads.txt",
+
+  networks: {
+    Admob: "ads/admob.txt",
+    Meta: "ads/meta.txt",
+    Applovin: "ads/applovin.txt",
+    Mintegral: "ads/mintegral.txt",
+    Liftoff: "ads/liftoff.txt",
+    Unity: "ads/unity.txt",
+    "Inmobi Admob": "ads/inmobi_admob.txt",
+    "Inmobi MAX": "ads/inmobi_max.txt"
+  }
+};
